da62aa9c8a62bc7e5fd2c3b13bcd5bdd28e5d1a3
GasyCoder <gasikaradigital@gmail.com>
2025-02-13 20:42:40 +0300

    fixed view auth

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -23,9 +23,16 @@
                 <x-label for="email" value="{{ __('Email') }}" />
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             </div>
+            <div class="flex justify-between mt-4">
+                <a href="{{ route('login') }}" wire:navigate
+                    class="inline-flex items-center px-4 py-2 bg-gray-500 hover:bg-red-600 text-white font-semibold rounded-md shadow-sm transition">
+                    <svg class="w-5 h-5 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                    {{ __('Cancel') }}
+                </a>
 
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
+                <x-button class="bg-blue-600 hover:bg-blue-700 text-white">
                     {{ __('Email Password Reset Link') }}
                 </x-button>
             </div>
